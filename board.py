e0148a9c67ddc407a4a10ee3573c474902795ad5
SVargo7 <149019304+SVargo7@users.noreply.github.com>
2023-12-06 20:25:08 -0500

    board.py

--- board.py
+++ board.py
@@ -1,91 +1,230 @@
-class Board:
+import pygame, sys
+pygame.init()
+from cell import Cell
+
+class Board():
+
+    # constructor
     def __init__(self, width, height, screen, difficulty):
-        self.width = width
-        self.height = height
-        self.square_size = width / 9
-        self.screen = screen
+        self.width = 450
+        self.height = 450
+        self.square_size = width/9
+        self.screen = pygame.display.set_mode((self.width, self.height))
         self.difficulty = difficulty
         self.cells = []
-        self.initial_values = []
-        self.selected_cell = None
-        size = 9
-        removed = self.difficulty
-        board = generate_sudoku(size, removed)
-        self.make_board(board)
+        self.selected_cell = Cell(0, 0, 0, self.screen)
 
-    def make_board(self, sodoku_board)
-        self.initial_values = [[sudoku_board[row][col] for col in range(9)] for row in range(9)]
-        for row in range(9):
-            for col in range(9):
-                cell = Cell(sudoku_board[row][col], row, col, self.screen)
-                self.cells.append(cell)
-                if sudoku_board[row][col] == 0:
-                    cell.editable = True
-                else:
-                    cell.editable = False
+        self.row1 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        self.row2 = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+        self.row3 = [18, 19, 20, 21, 22, 23, 24, 25, 26]
+        self.row4 = [27, 28, 29, 30, 31, 32, 33, 34, 35]
+        self.row5 = [36, 37, 38, 39, 40, 41, 42, 43, 44]
+        self.row6 = [45, 46, 47, 48, 49, 50, 51, 52, 53]
+        self.row7 = [54, 55, 56, 57, 58, 59, 60, 61, 62]
+        self.row8 = [63, 64, 65, 66, 67, 68, 69, 70, 71]
+        self.row9 = [72, 73, 74, 75, 76, 77, 78, 79, 80]
 
-    def reset_board(self):
-        for i, cell in enumerate(self.cells):
-            row = i // 9
-            col = i % 9
-            initial_value = self.initial_values[row][col]
-            cell.value = initial_value
-            cell.sketched_value = 0
-            cell.selected = False
-            if initial_value == 0:
-                cell.editable = True
-            else:
-                cell.editable = False
+        self.col1 = [0, 9, 18, 27, 36, 45, 54, 63, 72]
 
+
+
+    # draw function - DONE
     def draw(self):
-        # Draw each cell
+        # make background white
+        self.screen.fill("white")
+
+        # set lineWidth var
+        lineWidth = 0
+
+        # draw horizontal lines
+        for i in range(1, 9):
+            # set the bold for the 3x3 lines
+            if i == 3 or i == 6:
+                lineWidth = 6
+            else:
+                lineWidth = 2
+
+            # draw the line
+            pygame.draw.line(self.screen, "black", (0, i * self.square), (self.width, i * self.square), lineWidth)
+
+        # draw vertical lines
+        for i in range(1, 9):
+            # set the bold for the 3x3 lines
+            if i == 3 or i == 6:
+                lineWidth = 6
+            else:
+                lineWidth = 2
+
+            # draw the line
+            pygame.draw.line(self.screen, "black", (i * self.square, 0), (i * self.square, self.height), lineWidth)
+
+        # create the 81 cells and put them in the cell list
+        # name of cells is their index
+        for row in range(0, 9):
+            for col in range(0, 9):
+                self.cells.append(Cell(0, row, col, self.screen))
+
+
+
+    # select function - marks cell as current selected cell - DONE
+    def select(self, row, col):
+        ind = int(str(row)+str(col))
+        self.selected_cell = self.cells[ind]
+        (self.cells[ind]).draw()
+
+
+
+    # click function - returns row and col of coordinate - DONE
+    def click(self, x, y):
+        if x > self.width or y > self.height:
+            return None
+        else:
+            row = 0
+            col = 0
+
+            if x < self.square_size:
+                row = 1
+            elif x < 2*self.square_size:
+                row = 2
+            elif x < 3*self.square_size:
+                row = 3
+            elif x < 4*self.square_size:
+                row = 4
+            elif x < 5*self.square_size:
+                row = 5
+            elif x < 6*self.square_size:
+                row = 6
+            elif x < 7*self.square_size:
+                row = 7
+            elif x < 8*self.square_size:
+                row = 8
+
+
+            if y < self.square_size:
+                col = 1
+            elif y < 2*self.square_size:
+                col = 2
+            elif y < 3*self.square_size:
+                row = 3
+            elif y < 4*self.square_size:
+                col = 4
+            elif y < 5*self.square_size:
+                col = 5
+            elif y < 6*self.square_size:
+                col = 6
+            elif y < 7*self.square_size:
+                col = 7
+            elif y < 8*self.square_size:
+                col = 8
+
+            return (row, col)
+
+    # clear function - empty out selected cell - DONE
+    def clear(self):
+        self.selected_cell = ""
+
+
+
+    # sketch function - set value of sketched cell and draw it - DONE
+    def sketch(self, value):
+        self.selected_cell.set_sketched_value(value)
+        self.selected_cell.draw()
+
+
+
+    # sets value of cell equal to input value - DONE
+    def place_number(self, value):
+        self.selected_cell.set_cell_value(value)
+
+
+
+    # sets all cells to original value; 0 if cleared - DONE
+    def reset_to_original(self):
         for cell in self.cells:
-            cell.draw()
+            cell.set_sketched_value(cell.value)
 
-        # Draw grid lines
-        for x in range(0, self.width, int(self.square_size)):
-            pygame.draw.line(self.screen, BLACK, (x, 0), (x, self.height))
-        for y in range(0, self.height, int(self.square_size)):
-            pygame.draw.line(self.screen, BLACK, (0, y), (self.width, y))
 
-    def select_cell(self, pos):
+
+    # is full function - returns boolean of whether board full or not - DONE
+    def is_full(self):
+        try:
+            for cell in self.cells:
+                if 0 < cell.value <= 9:
+                    full = True
+                else:
+                    raise NameError
+            return full
+
+        except:
+            return False
+
+
+
+    # update board func - updates underlying 2d board with values in all cells
+    def update_board(self):
         for cell in self.cells:
-            if cell.is_clicked(pos) and cell.initially_empty and cell.editable:
-                if self.selected_cell:
-                    self.selected_cell.selected = False
+            cell.set_cell_value(cell.selected_value)
 
-                cell.selected = True
-                self.selected_cell = cell
-                return
-        if self.selected_cell:
-            self.selected_cell.selected = False
-            self.selected_cell = None
 
-    def handle_key_input(self, key):
-        if self.selected_cell and self.selected_cell.initially_empty:
 
-            if key >= pygame.K_1 and key <= pygame.K_9:
-                self.selected_cell.set_sketched_value(int(chr(key)))  # Set temporary value
+    # find empty func - returns row + col of an empty cell - DONE
+    def find_empty(self):
+        empty = ""
+        for cell in self.cells:
+            if cell.selected_value == 0:
+                empty = str(self.cells.index(cell))
+                break
+        row = 0
+        col = 0
 
-            elif key == pygame.K_RETURN and self.selected_cell.sketched_value != 0:
-                self.confirm_value()  # Confirm value when 'Enter' is pressed
+        if empty in self.row1:
+            row = 1
+            col = self.row1.index(empty)
+        elif empty in self.row2:
+            row = 2
+            col = self.row2.index(empty)
+        elif empty in self.row3:
+            row = 3
+            col = self.row3.index(empty)
+        elif empty in self.row4:
+            row = 4
+            col = self.row4.index(empty)
+        elif empty in self.row5:
+            row = 5
+            col = self.row5.index(empty)
+        elif empty in self.row6:
+            row = 6
+            col = self.row6.index(empty)
+        elif empty in self.row7:
+            row = 7
+            col = self.row7.index(empty)
+        elif empty in self.row8:
+            row = 8
+            col = self.row8.index(empty)
+        elif empty in self.row9:
+            row = 9
+            col = self.row9.index(empty)
 
-    def confirm_value(self):
-        if self.selected_cell:
-            self.selected_cell.set_cell_value(self.selected_cell.sketched_value)
-            self.selected_cell.selected = False
-            self.selected_cell = None
+        return (row, col)
 
-    def is_board_full(self):
-        return all(cell.value != 0 for cell in self.cells)
 
-    def is_board_correct(self):
-        # Check each row, column, and 3x3 square
-        for i in range(9):
-            row = [self.cells[i * 9 + j].value for j in range(9)]
-            col = [self.cells[j * 9 + i].value for j in range(9)]
-            square = [self.cells[3 * (i // 3) * 9 + 3 * (i % 3) + j // 3 * 9 + j % 3].value for j in range(9)]
 
-            if len(set(row)) != 9 or len(set(col)) != 9 or len(set(square)) != 9:
+    # checks if board is solved correctly - DONE
+    def check_board(self):
+        correct = False
+        for cell in self.cells:
+            if cell.selected_value == cell.value:
+                correct = True
+            else:
                 return False
-        return True
+        return correct
+
+
+
+# keep program running
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
+
